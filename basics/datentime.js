881e8ad3302218ff8a2c2f2aa4b1579400f2b0eb
Manjeetd7 <155341789+Manjeetd7@users.noreply.github.com>
2024-02-22 07:50:32 +0000

    Basics

---
+++ basics/datentime.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+let cDate = new Date (99,12,5,3,6, 16)
+console.log(cDate);
+console.log(cDate.toString());
+console.log(cDate.toLocaleString());
+console.log(cDate.toLocaleDateString());
